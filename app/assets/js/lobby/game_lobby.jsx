fd8e5b2ed950fd88c133d69aee14b6cc420f6a60
Steven Carroll <stevencarroll90@gmail.com>
2016-01-28 08:58:17 -0700

    Pushed the game lobby banner, title, and subtitle with minimal styling.

--- app/assets/js/lobby/game_lobby.jsx
+++ app/assets/js/lobby/game_lobby.jsx
@@ -1,10 +1,25 @@
+var GameBanner = React.createClass({
+   render: function() {
+	   var game = this.props.game;
+	   return (
+		   <div className="banner">
+			   <GameIcon game={game} color={"white"}/>
+		   </div>
+	   );
+   }
+});
+
 var GameLobby = React.createClass({
     render: function() {
     	var game = this.props.game;
         var title = this.props.title;
         return (
 	        <div className={"game-lobby-" + game}>
-	        	<GameIcon game={game} color={"white"}/>
+				<GameBanner game={game} />
+				<div className="container">
+					<h1 className="create-game">create game</h1>
+					<h2 className="enter">enter your name</h2>
+				</div>
 	        </div>
 	    );
     }
