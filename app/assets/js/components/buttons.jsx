3f8ecdc6ac0d6ff39fb3dfd7e5c59e08a2e9751d
Steven Carroll <stevencarroll90@gmail.com>
2016-02-03 18:50:43 -0700

    Set up a framework for passing a function into a button component to be executed on click.  Set the cancel button to return from the lobby to the game select screen

--- app/assets/js/components/buttons.jsx
+++ app/assets/js/components/buttons.jsx
@@ -1,9 +1,11 @@
 var LobbyButton = React.createClass({
-    render: function() {
+	render: function() {
     	var game = this.props.game;
     	var icon = this.props.icon;
     	var text = this.props.text;
         var hollow = this.props.hollow;
+
+		var handleClick = this.props.handleClick;
 
 		var className;
 		if (icon != null) {
@@ -20,7 +22,7 @@ var LobbyButton = React.createClass({
 		className += " " + game;
 
         return (
-	        <button className={className}>
+	        <button className={className} onClick={handleClick}>
 	        	{text}
 				{(() => {
 					if (icon != null) {
