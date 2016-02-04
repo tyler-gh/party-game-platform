aa6cb14b8607711f4204efc44f410c4419f9a774
Luke Dickinson <Lukeadickinson@yahoo.com>
2016-02-04 12:31:04 -0700

    basic functionally for the database is up. Can create,drop, reset tables. Can get the contents of the table as json object, and can insert items into the table

--- app/controllers/DatabaseController.scala
+++ app/controllers/DatabaseController.scala
@@ -1,11 +1,12 @@
 package controllers
 
-import models.Database
+import models.GameDB
+import models.UserDB
+import models.ActionDB
 import play.api.db.DB
 import anorm._
-import play.api.libs.json.Json
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
-
 //server can
 // reset all games, create game, join game, load all games
 
@@ -44,46 +45,91 @@ class DatabaseController extends Controller {
   //get actions to x
   //get action x
 
+  def insertGame(gameID: Int, joinCode : String, gameStatus : String) = Action {
+    val success = GameDB.insertRow(gameID, joinCode, gameStatus)
+    Ok(views.html.database_result("insert into games table", success.toString(), ""))
+  }
+
+  def insertUser(userID : Int, userName : String, gameID: Int) = Action {
+    val success = UserDB.insertRow(userID, userName, gameID)
+    Ok(views.html.database_result("insert into users table", success.toString(), ""))
+  }
+  def insertAction(actionNumber : Int, actionType : String, actionData : String, userID : Int, gameID : Int) = Action {
+    val success = ActionDB.insertRow(actionNumber, actionType, actionData, userID, gameID)
+    Ok(views.html.database_result("insert into actions table", success.toString(), ""))
+  }
+
+  def getGames = Action {
+    val games = GameDB.getRows()
+    Ok(games).as("application/json")
+  }
+
+  def getUsers = Action {
+    val games = UserDB.getRows()
+    Ok(games).as("application/json")
+  }
+
+  def getActions = Action {
+    val games = ActionDB.getRows()
+    Ok(games).as("application/json")
+  }
 
   def resetGamesDatabase = Action {
 
-    var success = Database.resetGamesTable()
+    var success = GameDB.resetTable()
 
-    Ok(views.html.database_result("reset Games Database", success.toString(), ""))
+    Ok(views.html.database_result("reset games Database", success.toString(), ""))
   }
   def resetUsersDatabase = Action {
 
-    var success = Database.resetUsersTable()
+    var success = UserDB.resetTable()
 
-    Ok(views.html.database_result("reset Games Database", success.toString(), ""))
+    Ok(views.html.database_result("reset users Database", success.toString(), ""))
   }
   def resetActionsDatabase = Action {
 
-    var success = Database.resetActionsTable()
+    var success = ActionDB.resetTable()
 
-    Ok(views.html.database_result("reset Games Database", success.toString(), ""))
+    Ok(views.html.database_result("reset actions Database", success.toString(), ""))
   }
   def resetEntireDatabase = Action {
 
-    var success1 = Database.resetGamesTable()
-    var success2 = Database.resetUsersTable()
-    var success3 = Database.resetActionsTable()
+    var success1 = GameDB.resetTable()
+    var success2 = UserDB.resetTable()
+    var success3 = ActionDB.resetTable()
 
-    var success = true
-    if(!success1 || !success2 || ! success3){
-      success = false
+    var success = false
+    if(success1 && success2 && success3){
+      success = true
     }
-    Ok(views.html.database_result("reset Games Database", success.toString(), ""))
+    Ok(views.html.database_result("reset entire Database", success.toString(), ""))
   }
   def createDatabaseTables = Action {
 
-    var success = Database.createDatabaseTables()
+    var success1 = GameDB.createTable()
+    var success2 = UserDB.createTable()
+    var success3 = ActionDB.createTable()
+
+    var success = false
+    if(success1 && success2 && success3){
+      success = true
+    }
     Ok(views.html.database_result("Create Database Tables ", success.toString(), ""))
   }
   def dropDatabaseTables = Action {
 
-    var success = Database.dropDatabaseTables()
+
+    var success3 = ActionDB.dropTable()
+    var success2 = UserDB.dropTable()
+    var success1 = GameDB.dropTable()
+
+    var success = false
+    if(success1 && success2 && success3){
+      success = true
+    }
+
     Ok(views.html.database_result("Drop Database Tables", success.toString(), ""))
   }
+
 
 }
---
+++ app/models/ActionDB.scala
@@ -0,0 +1,156 @@
+package models
+
+import java.sql.Date
+import java.util
+import java.util.Collections
+
+import anorm._
+import SqlParser._
+import play.api.db.DB
+import play.api.libs.json.Json
+import play.api.libs.json._
+import play.api.Play.current
+/**
+  * Created by Luke on 1/23/2016.
+  */
+//http://www.jamesward.com/2012/02/21/play-framework-2-with-scala-anorm-json-coffeescript-jquery-heroku
+//look here to start fixing my garbage
+case class ActionDB(actionNumber: Int,
+                    actionType: String,
+                    actionData: String,
+                    userID: Int,
+                    gameID:Int)
+
+object ActionDB {
+
+//  actionNumber  integer,
+//  actionType    text NOT NULL,
+//  actionData    text NOT NULL,
+//  userID        integer,
+//  gameID        integer,
+//  PRIMARY KEY(actionNumber, userID, gameID),
+//  FOREIGN KEY(userID) REFERENCES users(userID),
+//  FOREIGN KEY(gameID) REFERENCES games(gameID)
+
+  implicit object ActionDBFormat extends Format[ActionDB] {
+    // convert from ActionDB object to JSON (serializing to JSON)
+    def writes(actionDB: ActionDB): JsValue = {
+      val actionDBSeq = Seq(
+        "actionNumber" -> JsNumber(actionDB.actionNumber),
+        "actionType" -> JsString(actionDB.actionType),
+        "actionData" -> JsString(actionDB.actionData),
+        "userID" -> JsNumber(actionDB.userID),
+        "gameID" -> JsNumber(actionDB.gameID)
+      )
+      JsObject(actionDBSeq)
+    }
+
+    // convert from JSON string to a ActionDB object (de-serializing from JSON)
+    // (i don't need this method; just here to satisfy the api)
+    def reads(json: JsValue): JsResult[ActionDB] = {
+      JsSuccess(ActionDB(-1,"","",-1,-1))
+    }
+
+  }
+
+  val row = {
+      get[Int]("actionNumber") ~
+      get[String]("actionType") ~
+      get[String]("actionData") ~
+      get[Int]("userID") ~
+      get[Int]("gameID") map {
+      case actionNumber ~ actionType ~ actionData ~ userID ~ gameID =>
+        ActionDB(actionNumber,actionType,actionData, userID, gameID)
+    }
+  }
+
+  def convertToJson(actions: Seq[ActionDB]): JsValue = {
+    Json.toJson(actions)
+  }
+
+  def getRows(): JsValue = {
+    DB.withConnection { implicit connection =>
+      var result = SQL("select * from actions").as(ActionDB.row *)
+      convertToJson(result)
+    }
+  }
+
+  def insertRow(actionNumber : Int, actionType : String, actionData : String, userID : Int, gameID : Int): Boolean = {
+    var success = false
+    var result : Int = -1
+    DB.withConnection { implicit connection =>
+      result = SQL(
+        s"insert into actions(actionNumber, actionType, actionData, userID, gameID) " +
+        s"values ('$actionNumber', '$actionType', '$actionData','$userID','$gameID')")
+        .executeUpdate()
+    }
+    if(result == -1){
+      //todo throw error, not return
+      return false
+    }
+
+    if(result == 1) {
+      success = true
+    }
+    return success
+  }
+
+
+
+  def resetTable(): Boolean = {
+    var success = true;
+    var result: Int = -1;
+    DB.withConnection { implicit connection =>
+      result = SQL("DELETE FROM actions").executeUpdate()
+    }
+    if(result == -1){
+      //todo throw error, not return
+      return false
+    }
+
+    if(result != 0) {
+      success = true
+    }
+
+    return success
+  }
+
+  def createTable(): Boolean  = {
+    var success = false;
+    var result: Boolean = false;
+    DB.withConnection { implicit connection =>
+      result = SQL(
+        """
+          CREATE TABLE IF NOT EXISTS actions (
+          actionNumber  integer,
+          actionType    text NOT NULL,
+          actionData    text NOT NULL,
+          userID        integer,
+          gameID        integer,
+          PRIMARY KEY(actionNumber, userID, gameID),
+          FOREIGN KEY(userID) REFERENCES users(userID),
+          FOREIGN KEY(gameID) REFERENCES games(gameID)
+          )
+        """
+      ).execute()
+    }
+    if(result == false) {
+      success = true
+    }
+
+    return success
+  }
+
+  def dropTable(): Boolean  = {
+    var success = false;
+    var result: Boolean = false
+    DB.withConnection { implicit connection =>
+      result = SQL("DROP TABLE actions CASCADE;").execute()
+    }
+    if(result == false) {
+      success =true
+    }
+    return success
+  }
+
+}
