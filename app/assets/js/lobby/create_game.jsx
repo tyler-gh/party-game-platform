c346584532171adc8b3131487f1d8f257af82590
Steven Carroll <stevencarroll90@gmail.com>
2016-02-08 15:14:23 -0700

    Added a game-created-success page, bound it to the create-button's click function.

--- app/assets/js/lobby/create_game.jsx
+++ app/assets/js/lobby/create_game.jsx
@@ -19,6 +19,11 @@ var GameCreator = React.createClass({
 			ReactDOM.render(<GameLobby game={game} title={title} description={description} />, document.getElementById('pg-app'));
 		};
 
+		var clickCreate = function() {
+			ReactDOM.render(<GameCreatedSuccess game={game} title={title} />, document.getElementById('pg-app'));
+		};
+
+
         return (
 	        <div>
 				<GameBanner game={game} />
@@ -27,11 +32,36 @@ var GameCreator = React.createClass({
 						<h1 className="create-game">create game</h1>
 						<LobbyForm game={game} instructions="enter your name"/>
 						<h3 className="create-game">select your color</h3>
-						<LobbyButton game={game} text={"create"}/>
+						<LobbyButton game={game} text={"create"} handleClick={clickCreate} />
 						<LobbyButton game={game} hollow="color" text={"cancel"} handleClick={clickCancel} />
 					</div>
 				</LobbyContainer>
 	        </div>
 	    );
     }
+});
+
+var GameCreatedSuccess = React.createClass({
+	render: function() {
+		var game = this.props.game;
+		var title = this.props.title;
+		var description = this.props.description;
+
+		var clickCancel = function() {
+			ReactDOM.render(<GameLobby game={game} title={title} description={description} />, document.getElementById('pg-app'));
+		};
+
+		return (
+			<div>
+				<GameBanner game={game} />
+				<LobbyContainer game={game} color="color">
+					<div className="container">
+						<h1 className="success">success!</h1>
+						<h3 className="success">{"Your " + title + " game was successfully created.  Give friends access with the following code:"}</h3>
+						<h1 className="game-code">475YZ</h1>
+					</div>
+				</LobbyContainer>
+			</div>
+		);
+	}
 });
