2d8402354d7b2ed0b6505c8e1de546aee8bdb17c
Steven Carroll <stevencarroll90@gmail.com>
2016-02-11 09:54:17 -0700

    Waiting room shell, no players yet.

--- app/assets/js/lobby/waiting_room.jsx
+++ app/assets/js/lobby/waiting_room.jsx
@@ -3,13 +3,14 @@ var WaitingRoom = React.createClass({
     	var game = this.props.game;
 		var title = this.props.title;
 		var description = this.props.description;
+		var gameCode = this.props.gameCode;
 
-		var clickCancel = function() {
-			ReactDOM.render(<GameLobby game={game} title={title} description={description}/>, document.getElementById('pg-app'));
+		var clickLeave = function() {
+			ReactDOM.render(<GameUserJoin game={game} title={title} description={description} gameCode={gameCode}/>, document.getElementById('pg-app'));
 		};
 
-		var clickJoin = function() {
-			ReactDOM.render(<GameUserJoin game={game} title={title} description={description}/>, document.getElementById('pg-app'));
+		var clickStart = function() {
+			//ReactDOM.render(<GameUserJoin game={game} title={title} description={description}/>, document.getElementById('pg-app'));
 		};
 
         return (
@@ -17,10 +18,10 @@ var WaitingRoom = React.createClass({
 				<GameBanner game={game} />
 				<LobbyContainer game={game} color="color">
 					<div className="container">
-						<h1 className="create-game">join game</h1>
-						<LobbyForm game={game} instructions="enter game code"/>
-						<LobbyButton game={game} text={"submit"} handleClick={clickJoin} />
-						<LobbyButton game={game} hollow="color" text={"cancel"} handleClick={clickCancel} />
+						<h1 className="waiting-room">waiting for players</h1>
+						<h3 className="waiting-room">{"game code: " + gameCode}</h3>
+						<LobbyButton game={game} text={"start"} handleClick={clickStart} />
+						<LobbyButton game={game} hollow="color" text={"leave"} handleClick={clickLeave} />
 					</div>
 				</LobbyContainer>
 	        </div>
